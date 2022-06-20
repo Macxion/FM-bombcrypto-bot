12379d193ce3e5af2f7206024550f8304206d021
Macxion <macxionallan@gmail.com>
2022-06-20 00:03:53 -0300

    update de banco

--- conf.py
+++ conf.py
@@ -11,16 +11,14 @@ threshold = 0.7
 # o tempo em segundos, calcule um tempo legal em que todos os seus bhero se recuperam
 # e podem voltar ao trabalho, todos de uma vez, o padrão é 4400 segundos (1h e 22m)
 # !APENAS VALORES NUMÉRICOS
-# time_to_rest = 4400
-time_to_rest = 90
+time_to_rest = 4400
 
 # Delay para logar novamente e fugir do erro das pedras com vida infinita (uhmm)
 # e também salvar algum eventual bhero bugado no mapa, antes de dar o refresh, o bot
 # vai voltar na tela inicial para evitar o bug do rollback (uhmm²), sempre informe o
 # tempo em segundos, 360 segundos (6m) é o padrão
 # !APENAS VALORES NUMÉRICOS
-# time_to_refresh = 360
-time_to_refresh = 60
+ time_to_refresh = 360
 
 # Número de tentavivas para procurar uma imagem requerida, se o bot não encontrar a
 # imagem que procura, irá reinciar o processo novamente (recarregar o browser, conectar
@@ -34,7 +32,7 @@ max_attempt_found_img = 30
 # canais oficiais e veja o procedimento, no grupo https://t.me/BombCryptoBR você
 # encontra o passo a passo na mensagem fixada #33, o padrão é True
 # !APENAS VALORES BOOLEANOS, True OU False, SEM ASPAS
-login_with_metamask = False
+login_with_metamask = True
 
 # Se optou por NÃO fazer login utilizando a Metamask e já criou seu login e senha do
 # Bomcrypto, basta substituir os valores em username e password, MUITA ATENÇÃO se
@@ -46,7 +44,7 @@ login_with_metamask = False
 # fornecidos
 # !APENAS VALORES DE TEXTO, COM ASPAS, ESSA VARIÁVEL É UM DICTIONARY, MANTENHA O PADRÃO
 login_credentials = {
-    'macxion1': '01314011',
+    '': '',
 }
 
 # Se quer receber notificações do bot pelo telegram, sugerimos habilitar o envio de
@@ -60,7 +58,7 @@ telegram_notify = True
 # ao falar com o @BotFather, no próprio telegram mesmo, igual o site oficial orienta
 # link https://core.telegram.org/bots#3-how-do-i-create-a-bot
 # !APENAS VALORES DE TEXTO, COM ASPAS
-bot_token = '600166001:AAFjtJoIbv7VZHcsA64hgNxnAOAhLMRB5Bk'
+bot_token = ''
 
 # Se decidiu receber notificações por telegram e informou o bot_token, você também deve
 # informa o chat_id, ele é o Id da sua conversa com o bot, para descobrir seu chatId,
@@ -68,7 +66,7 @@ bot_token = '600166001:AAFjtJoIbv7VZHcsA64hgNxnAOAhLMRB5Bk'
 # substituindo o <token> pelo token recebido do @BotFather, procure pela chave "id" no json
 # recebido, esse é o chatId
 # !APENAS VALORES DE TEXTO, COM ASPAS
-chat_id = '593069474'
+chat_id = ''
 
 # Informe a partir de que horário (formato 24h, ex: 15:30) deseja que o bot envie um print da
 # quantia total de bcoin até o momento, pelo Telegram, apenas uma vez, a cada 24h
